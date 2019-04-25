e0c9ddc8647a1be67b03417e78091c21aa71a684
cmcca <cata92.mh@gmail.com>
2019-04-25 17:41:10 -0400

    working on API

--- app/server.js
+++ app/server.js
@@ -1,13 +1,13 @@
 const express = require("./node_modules/express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Configure body parsing for AJAX requests
+// Cparsing for AJAX
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));

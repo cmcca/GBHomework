f81d1b01b54d9ded3aff699a9e369c7579286d93
cmcca <cata92.mh@gmail.com>
2019-04-25 15:21:13 -0400

    issues

--- app/routes/api/books.js
+++ app/routes/api/books.js
@@ -1,4 +1,4 @@
-const router = require("express").Router();
+const router = require("../../node_modules/express").Router();
 const bookController = require("../../controllers/bookController");
 
 // Matches with "/api/books"
